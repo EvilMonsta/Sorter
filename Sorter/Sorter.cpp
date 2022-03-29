5ee1ab863e7b51455c611f223cf0a01220354220
EvilMonsta <etoxroman@gmail.com>
2022-03-29 21:58:24 +0300

    Other disiplaying

--- Sorter/Sorter.cpp
+++ Sorter/Sorter.cpp
@@ -21,10 +21,11 @@ int main(int argc, char* argv[])
             return 0;
         }
     }
-    char ch;
-    while (in.get(ch))
+
+    while (!in.eof())
     {
-        cout << ch;
+        getline(in, s);
+        cout << s << endl;
     }
     return 0;
 }
