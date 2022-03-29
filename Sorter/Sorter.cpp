99f162c8d20fb859fb6ebc53b2bec72a86576902
EvilMonsta <etoxroman@gmail.com>
2022-03-29 21:31:45 +0300

    Displaying content on the screen

--- Sorter/Sorter.cpp
+++ Sorter/Sorter.cpp
@@ -1,10 +1,30 @@
 ﻿#include <iostream>
-
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Hello World:)";
-
+    int i;
+    string s;
+    ifstream in;
+    if (argc < 2)
+    {
+        cout << "Write something or use console, if u don't use it";
+        return 0;
+    }
+    else
+    {
+        in.open(argv[1]);
+    if (!in.is_open()) {
+            cerr << "Call the program in a new way and type the correct path" << endl;
+            return 0;
+        }
+    }
+    char ch;
+    while (in.get(ch))
+    {
+        cout << ch;
+    }
     return 0;
 }
